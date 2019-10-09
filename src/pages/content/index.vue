f8185eb34c973b76653a6e760f8cb9600f832c29
Zzhangyt <levi.zhang@enesource.com>
2019-10-09 11:02:30 +0800

    three

---
+++ src/pages/content/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+  import * as THREE from 'three'
+    export default {
+        name: "index",
+        data(){
+            return{
+
+            }
+        },
+        mounted(){
+            this.init()
+        },
+        methods:{
+            init(){
+                let scene = new THREE.Scene();
+                let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+                let renderer = new THREE.WebGLRenderer();
+                let geometry = new THREE.BoxGeometry( 5, 5, 5 );
+                let material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+                let cube = new THREE.Mesh( geometry, material );
+                scene.add( cube );
+                camera.position.z = 5;
+                renderer.setSize( window.innerWidth, window.innerHeight);
+                document.body.appendChild( renderer.domElement );
+                function animate() {
+                    requestAnimationFrame( animate );
+                    cube.rotation.x += 0.03;
+                    cube.rotation.y += 0.03;
+                    renderer.render( scene, camera );
+                }
+                animate()
+            },
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  canvas { width: 100%; height: 100%;}
+</style>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -8,6 +8,13 @@
         <button @click="reduce">reduce</button>
         <button @click="save">save</button>
         <button @click="take">take</button>
+        <ul style="width: 500px;height: 300px;background: #f4f4f4">
+          <li v-for="(item,index) in list" :key="index">
+            <input type="text" v-model="item.value">
+            <input type="text" v-model="item.value2">
+            <button @click="btn(item)">btn</button>
+          </li>
+        </ul>
       </div>
     </div>
 </template>
@@ -18,15 +25,29 @@
         data(){
             return{
                 text:this.$store.state.count,
-                count:100
+                count:100,
+                list:[
+                    {
+                        value:'',
+                        value2:''
+                    }
+                ]
             }
         },
         created() {
-
+            // this.animation()
         },
         methods:{
+            btn(val){
+              console.log(val)
+            },
             add(){
-                this.$store.commit('add',10)
+                // this.$store.commit('add',10)
+                let obj = {
+                    value:'',
+                    value2:''
+                };
+                this.list.push(obj)
             },
             reduce(){
                 this.$store.commit('reduce',5)
@@ -36,6 +57,84 @@
             },
             take(){
                 alert(`值为:${this.$store.state.value}`);
+            },
+            animation(){
+                !function() {
+                    function o(w, v, i) {
+                        return w.getAttribute(v) || i
+                    }
+                    function j(i) {
+                        return document.getElementsByTagName(i)
+                    }
+                    function l() {
+                        var i = j("script"),
+                            w = i.length,
+                            v = i[w - 1];
+                        return {
+                            l: w,
+                            z: o(v, "zIndex", -1),
+                            o: o(v, "opacity", 1),
+                            c: o(v, "color", "red"),
+                            n: o(v, "count", 99)
+                        }
+                    }
+                    function k() {
+                        r = u.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, n = u.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+                    }
+                    function b() {
+                        e.clearRect(0, 0, r, n);
+                        var w = [f].concat(t);
+                        var x, v, A, B, z, y;
+                        t.forEach(function(i) {
+                            i.x += i.xa, i.y += i.ya, i.xa *= i.x > r || i.x < 0 ? -1 : 1, i.ya *= i.y > n || i.y < 0 ? -1 : 1, e.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
+                            for (v = 0; v < w.length; v++) {
+                                x = w[v];
+                                if (i !== x && null !== x.x && null !== x.y) {
+                                    B = i.x - x.x, z = i.y - x.y, y = B * B + z * z;
+                                    y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, e.beginPath(), e.lineWidth = A / 2, e.strokeStyle = "rgba(" + s.c + "," + (A + 0.2) + ")", e.moveTo(i.x, i.y), e.lineTo(x.x, x.y), e.stroke())
+                                }
+                            }
+                            w.splice(w.indexOf(i), 1)
+                        }), m(b)
+                    }
+                    var u = document.createElement("canvas"),
+                        s = l(),
+                        c = "c_n" + s.l,
+                        e = u.getContext("2d"),
+                        r, n, m = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+                        function(i) {
+                            window.setTimeout(i, 1000 / 45)
+                        }, a = Math.random, f = {
+                            x: null,
+                            y: null,
+                            max: 20000
+                        };
+                    u.id = c;
+                    u.style.cssText = "position:fixed;top:0;left:0;z-index:" + s.z + ";opacity:" + s.o;
+                    j("body")[0].appendChild(u);
+                    k(), window.onresize = k;
+                    window.onmousemove = function(i) {
+                        i = i || window.event, f.x = i.clientX, f.y = i.clientY
+                    }, window.onmouseout = function() {
+                        f.x = null, f.y = null
+                    };
+                    for (var t = [], p = 0; s.n > p; p++) {
+                        var h = a() * r,
+                            g = a() * n,
+                            q = 2 * a() - 1,
+                            d = 2 * a() - 1;
+                        t.push({
+                            x: h,
+                            y: g,
+                            xa: q,
+                            ya: d,
+                            max: 6000
+                        })
+                    }
+                    setTimeout(function() {
+                        b()
+                    }, 100)
+                }();
             }
         }
     }
